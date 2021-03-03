262b6b446fbb57a284d509348d5d0281c18b2196
Jesse <jbenlind@gmail.com>
2021-03-03 11:03:52 -0600

    finished nav

--- react-app/src/components/Navigation/navigation.js
+++ react-app/src/components/Navigation/navigation.js
@@ -6,18 +6,23 @@ const Navigation = () => {
 
     return (
         <>
-            <div id="container">
-                {/* all caps? */}
-
-                <NavLink id="bloom-title" to="">
-                    <span id="b">B</span>
-                    <span id="loom">loom</span>
-                </NavLink>
-                <NavLink className="link" to="">Templates</NavLink>
-                <NavLink className="link" to="">Find a page</NavLink>
-                <NavLink className="link" to="">Log In</NavLink>
-                {/* <NavLink className="link" to="">My page</NavLink> */}
-                <NavLink id="getStarted-button" to="">Get Started</NavLink>
+            <div id="grid-container">
+                <div id="first-fraction">
+                    <NavLink id="bloom-title" to="">
+                        <span id="b">B</span>
+                        <span id="loom">loom</span>
+                    </NavLink>
+                </div>
+                <div id="second-fraction">
+                    <NavLink className="link" to="">Templates</NavLink>
+                    <NavLink className="link" to="">Find a page</NavLink>
+                    <NavLink className="link" to="">Log In</NavLink>
+                    {/* <NavLink className="link" to="">My page</NavLink> */}
+                </div>
+                <div id="third-fraction">
+                    {/* <Logout /> */}
+                    <NavLink id="getStarted-button" to="">Get Started</NavLink>
+                </div>
             </div>
 
         </>
