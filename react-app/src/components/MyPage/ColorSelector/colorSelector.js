cc5702634c2889f75630e160408356e5eb837017
Jesse <jbenlind@gmail.com>
2021-03-14 19:53:14 -0500

    added slider

--- react-app/src/components/MyPage/ColorSelector/colorSelector.js
+++ react-app/src/components/MyPage/ColorSelector/colorSelector.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./colorSelector.css";
 
 const ColorSelector = ({}) => {
+
+    const [selected, setSelected] = useState("");
+    const [previous, setPrevious] = useState("left")
+
+    const slideSelector = (param) => {
+        if(param === "right" && previous === "left") {
+            setSelected("slide-right");
+            setPrevious("right")
+        } else if(param === "left" && previous === "right") {
+            setSelected("slide-left")
+            setPrevious("left")
+        }
+    }
+
     return (
         <>
+            <div id={selected} className="slider"></div>
             <div className="colorSelector-section">
-                <div className="standard">
+                <div onClick={(e) => slideSelector("left")} className="standard">
                     <div className="first-standard"></div>
                     <div className="second-standard"></div>
                 </div>
                 <div></div>
-                <div className="colored">
+                <div onClick={(e) => slideSelector("right")} className="colored">
                     <div className="first-color"></div>
                     <div className="second-color"></div>
                 </div>
             </div>
+            <div className="slider-container"></div>
         </>
     )
 }
